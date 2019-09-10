0b1c4e6900f966c8907b632d163720e756062c8d
git-pusher <rachel.palomeque@gmail.com>
2019-09-10 18:32:41 -0500

    calculadora

---
+++ src/components/Calculadora.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+
+class Calculadora extends React.Component {
+    constructor(props) {
+		super(props);
+		this.state = {}
+    }
+    
+    render () {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Calculadora;
